b71cb93db9440f252ccb112860b58c01b950175f
Robert Eriksson <roberteriksson@roberts-air.mynet>
2018-11-11 12:42:33 +0100

    Add Animation bounce in and out to question text

--- team_health_quiz/lib/UI/question_text.dart
+++ team_health_quiz/lib/UI/question_text.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
-class QuestionText extends StatefulWidget{
+class QuestionText extends StatefulWidget {
+
+  final String _question;
+  final int _questionNumber;
+
+  QuestionText(this._question, this._questionNumber);
+
   @override
   State createState() => new QuestionTextState();
 }
 
-class QuestionTextState extends State<QuestionText> {
+class QuestionTextState extends State<QuestionText> with SingleTickerProviderStateMixin {
+
+  Animation<double> _fontSizeAnimation;
+  AnimationController _fontSizeAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _fontSizeAnimationController = new AnimationController(
+      value: 0.1,
+      duration: new Duration(seconds: 3),
+      lowerBound: 0.0,
+      upperBound: 1.0,
+      vsync: this);
+    _fontSizeAnimation = new CurvedAnimation(parent: _fontSizeAnimationController, curve: Curves.bounceOut);
+    _fontSizeAnimation.addListener(() => this.setState(() {}));
+    _fontSizeAnimationController.forward();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Material(
       color: Colors.white,
       child: new Padding(
-        padding: new EdgeInsets.symmetric(vertical: 200.0),
+        padding: new EdgeInsets.symmetric(vertical: 40.0),
         child: new Center(
-          child: new Text("Easy to delivery"),
-        ),
-      )
+          child: new Text("Statement " + widget._questionNumber.toString() + ": " + widget._question,
+            style: new TextStyle(
+              color: Colors.indigo,
+              fontSize: _fontSizeAnimation.value * 20),
+          ),
+        )
+      ),
     );
   }
 }
--- team_health_quiz/lib/pages/form_page.dart
+++ team_health_quiz/lib/pages/form_page.dart
@@ -7,24 +7,23 @@ import '../UI/question_text.dart';
 
 class FormPage extends StatefulWidget {
   @override
-  State createState() => FormPageState();
+  State createState() => new FormPageState();
 }
 
 class FormPageState extends State<FormPage> {
   @override
-    Widget build(BuildContext contrext) {
-      return new Stack(
-        children: <Widget>[
-          new Column( // THis is the main page
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new QuestionText(),
-              new AnswerButton(Colors.redAccent, 'Sad face'),
-              new AnswerButton(Colors.yellowAccent, 'Ok face'),
-              new AnswerButton(Colors.lightGreenAccent, 'Happy face'),
-            ],
-          )
-        ],
-      );
-    }
+  Widget build(BuildContext contrext) {
+    return new Stack(
+      children: <Widget>[
+        new Column( // THis is the main page
+          children: <Widget>[
+            new QuestionText("It is easy to release", 5),
+            new AnswerButton(Colors.redAccent, 'Sad face'),
+            new AnswerButton(Colors.yellowAccent, 'Ok face'),
+            new AnswerButton(Colors.lightGreenAccent, 'Happy face'),
+          ],
+        )
+      ],
+    );
+  }
 }
